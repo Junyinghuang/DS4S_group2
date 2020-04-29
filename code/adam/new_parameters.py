771b22999858c85b023738c46e44e055cbb33a67
Adam Kunesh <apkunesh@ucdavis.edu>
2020-04-28 18:41:45 -0700

    fixing up new_parameters.py

--- code/adam/new_parameters.py
+++ code/adam/new_parameters.py
@@ -8,6 +8,7 @@ def get_new_parameters(list_of_sigmas,list_of_recent_guesses):
     old_guesses = [2,3]
     sigmas = [1,1]
     get_new_parameters(sigmas,old_guesses)
+    
     '''
     new_guesses = [np.random.normal(point,sigma) for point,sigma in zip(list_of_recent_guesses,list_of_sigmas)]
     return new_guesses
