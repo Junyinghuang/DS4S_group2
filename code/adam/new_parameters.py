f092df22db6bfc7befd8ac612b761922a0435e1f
Adam Kunesh <apkunesh@ucdavis.edu>
2020-04-28 17:29:53 -0700

    implementing random walk and constraining runme

--- code/adam/new_parameters.py
+++ code/adam/new_parameters.py
@@ -1,1 +1,13 @@
 # For making jumps in parameter space
+
+import numpy as np
+
+def get_new_parameters(list_of_sigmas,list_of_recent_guesses):
+    '''
+    Ex:
+    old_guesses = [2,3]
+    sigmas = [1,1]
+    get_new_parameters(sigmas,old_guesses)
+    '''
+    new_guesses = [np.random.normal(point,sigma) for point,sigma in zip(list_of_recent_guesses,list_of_sigmas)]
+    return new_guesses
--- runme.py
+++ runme.py
@@ -1,1 +1,40 @@
 # A simple file to produce the results we'll present on 05/07/20
+
+def reproduce_pantheon_constraints():
+    pass
+
+def plot_posterior_of_h0():
+    pass
+
+def residual_plot_binned_data():
+    pass
+
+def chi_squared_and_PTE():
+    pass
+
+def noise_and_best_fit():
+    pass
+
+def histogram_of_cov_squared_residuals():
+    pass
+
+def h0_stability_test():
+    pass
+
+#Part 1
+reproduce_pantheon_constraints()
+
+
+#Part 2
+plot_posterior_of_h0()
+
+#Part 3, tests
+residual_plot_binned_data()
+
+chi_squared_and_PTE()
+
+noise_and_best_fit()
+
+histogram_of_cov_squared_residuals()
+
+h0_stability_test()
