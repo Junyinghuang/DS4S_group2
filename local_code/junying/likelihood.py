578136ae0bfc73752377be255a69ab9a580bcbd2
Junying Huang <xxzdkg@gmail.com>
2020-05-02 16:49:04 -0700

    speed up

--- local_code/junying/likelihood.py
+++ local_code/junying/likelihood.py
@@ -1,27 +1,6 @@
 import numpy as np
 import gmpy2
 
-def z(i):
-    file = open("local_code/junying/z.txt","r")
-    a=[]
-    for line in file:
-        a.append(float(line.strip('\n')))
-    return a[i]
-
-def mb(i):
-    file = open("local_code/junying/mb.txt","r")
-    a=[]
-    for line in file:
-        a.append(float(line.strip('\n')))
-    return a[i]
-
-def dmb(i):
-    file = open("local_code/junying/dmb.txt","r")
-    a=[]
-    for line in file:
-        a.append(float(line.strip('\n')))
-    return a[i]
-
 def E(z, o_l, o_m):
     o_k=1-o_l-o_m
     return np.sqrt(o_m*(1+z)**3+o_l+abs(o_k)*(1+z)**2)
@@ -40,32 +19,46 @@ def dL(z, o_l, o_m, H0):
         return (c/H0)*((1+z)/np.sqrt(o_k))*np.sin(np.sqrt(o_k)*inti)
 
 def mu(z, o_l, o_m, H0):
-    return 5*np.log(dL(z, o_l, o_m, H0)/10)+30
-
-def csys(i):
-    file = open("local_code/junying/csys.txt","r")
-    a=[]
-    for line in file:
-        a.append(float(line.strip('\n')))
-    return a[i]
+    return 5*np.log10(dL(z, o_l, o_m, H0)/10)+30
 
 def gaus(mean, sigma, x):
     return np.exp(-(((x-mean)/sigma)**2)/2)/sigma/np.sqrt(2*np.pi)
 
-#Producing cinv, used in likelihood below
-b=np.zeros(shape=(40,40))
-for i in range(0,40):
-    for j in range(0,40):
-        k=40*i+j
-        b[i,j]=csys(k)
-for i in range(0,40):
-    b[i,i]=b[i,i]+dmb(i)
-cinv=np.linalg.inv(b)
-
 def likelihood(o_l, o_m, H0, M):
+    
+    file = open("z.txt","r")
+    z=[]
+    for line in file:
+        z.append(float(line.strip('\n')))
+    file.close()    
+    
+    file = open("mb.txt","r")
+    mb=[]
+    for line in file:
+        mb.append(float(line.strip('\n')))
+    file.close()
+    
+    file = open("dmb.txt","r")
+    dmb=[]
+    for line in file:
+        dmb.append(float(line.strip('\n')))
+    file.close()
+    
+    file = open("csys.txt","r")
+    csys=[]
+    for line in file:
+        csys.append(float(line.strip('\n')))
+    c=np.zeros(shape=(40,40))
+    for i in range(0,40):
+        for j in range(0,40):
+            k=40*i+j
+            c[i,j]=csys[k]
+    for i in range(0,40):
+        c[i,i]=c[i,i]+dmb[i]
+    cinv=np.linalg.inv(c)
     expo=0
     for i in range(0,40):
-        first_term = (mb(i)-(mu(z(i),o_l, o_m, H0)+M))
         for j in range(0,40):
-            expo=expo-first_term*cinv[i,j]*(mb(j)-(mu(z(j),o_l, o_m, H0)+M))/2
-    return gmpy2.exp(expo)*gaus(19.23,0.042,M)
+            expo=expo-(mb[i]-(mu(z[i],o_l, o_m, H0)+M))*cinv[i,j]*(mb[j]-(mu(z[j],o_l, o_m, H0)+M))/2
+    return gmpy2.exp(expo)*gaus(-19.23,0.042,M)
+
